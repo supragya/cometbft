6a2efd92d69069a58508322df69863ee7493daa6
Mikhail Zabaluev <mikhail@informal.systems>
2023-03-16 14:45:04 +0200

    abci: more proto type aliases

--- abci/types/types.go
+++ abci/types/types.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"encoding/json"
+	"github.com/cosmos/gogoproto/grpc"
 
 	"github.com/cometbft/cometbft/abci/types/v1"
 	"github.com/cometbft/cometbft/abci/types/v2"
@@ -26,6 +27,24 @@ type RequestExtendVote = v3.RequestExtendVote
 type RequestVerifyVoteExtension = v3.RequestVerifyVoteExtension
 type RequestFinalizeBlock = v3.RequestFinalizeBlock
 
+// Discriminated Request variants are defined in the latest proto package.
+type Request_Echo = v3.Request_Echo
+type Request_Flush = v3.Request_Flush
+type Request_Info = v3.Request_Info
+type Request_InitChain = v3.Request_InitChain
+type Request_Query = v3.Request_Query
+type Request_CheckTx = v3.Request_CheckTx
+type Request_Commit = v3.Request_Commit
+type Request_ListSnapshots = v3.Request_ListSnapshots
+type Request_OfferSnapshot = v3.Request_OfferSnapshot
+type Request_LoadSnapshotChunk = v3.Request_LoadSnapshotChunk
+type Request_ApplySnapshotChunk = v3.Request_ApplySnapshotChunk
+type Request_PrepareProposal = v3.Request_PrepareProposal
+type Request_ProcessProposal = v3.Request_ProcessProposal
+type Request_ExtendVote = v3.Request_ExtendVote
+type Request_VerifyVoteExtension = v3.Request_VerifyVoteExtension
+type Request_FinalizeBlock = v3.Request_FinalizeBlock
+
 type Response = v3.Response
 type ResponseException = v1.ResponseException
 type ResponseEcho = v1.ResponseEcho
@@ -45,10 +64,44 @@ type ResponseExtendVote = v3.ResponseExtendVote
 type ResponseVerifyVoteExtension = v3.ResponseVerifyVoteExtension
 type ResponseFinalizeBlock = v3.ResponseFinalizeBlock
 
-type ExecTxResult = v3.ExecTxResult
-type EventAttribute = v2.EventAttribute
+// Discriminated Response variants are defined in the latest proto package.
+type Response_Exception = v3.Response_Exception
+type Response_Echo = v3.Response_Echo
+type Response_Flush = v3.Response_Flush
+type Response_Info = v3.Response_Info
+type Response_InitChain = v3.Response_InitChain
+type Response_Query = v3.Response_Query
+type Response_CheckTx = v3.Response_CheckTx
+type Response_Commit = v3.Response_Commit
+type Response_ListSnapshots = v3.Response_ListSnapshots
+type Response_OfferSnapshot = v3.Response_OfferSnapshot
+type Response_LoadSnapshotChunk = v3.Response_LoadSnapshotChunk
+type Response_ApplySnapshotChunk = v3.Response_ApplySnapshotChunk
+type Response_PrepareProposal = v3.Response_PrepareProposal
+type Response_ProcessProposal = v3.Response_ProcessProposal
+type Response_ExtendVote = v3.Response_ExtendVote
+type Response_VerifyVoteExtension = v3.Response_VerifyVoteExtension
+type Response_FinalizeBlock = v3.Response_FinalizeBlock
 
+type ExecTxResult = v3.ExecTxResult
+type Event = v2.Event
+type EventAttribute = v2.EventAttribute
+type Misbehavior = v2.Misbehavior
+type MisbehaviorType = v2.MisbehaviorType
+type TxResult = v3.TxResult
 type ValidatorUpdate = v1.ValidatorUpdate
+
+type ABCIClient = v3.ABCIClient
+
+func NewABCIClient(cc grpc.ClientConn) ABCIClient {
+	return v3.NewABCIClient(cc)
+}
+
+const (
+	MisbehaviorType_UNKNOWN             MisbehaviorType = v2.MisbehaviorType_UNKNOWN
+	MisbehaviorType_DUPLICATE_VOTE      MisbehaviorType = v2.MisbehaviorType_DUPLICATE_VOTE
+	MisbehaviorType_LIGHT_CLIENT_ATTACK MisbehaviorType = v2.MisbehaviorType_LIGHT_CLIENT_ATTACK
+)
 
 type ResponseProcessProposal_ProposalStatus = v2.ResponseProcessProposal_ProposalStatus
 
